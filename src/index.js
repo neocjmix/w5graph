ffecb6a2a929899bb35bb1ac90aa98ac6ba3a4e5
Chanjin Park <neocjmix@gmail.com>
2018-06-08 03:10:48 +0900

    apply middleware

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux'
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
+import ReduxLogger from 'redux-logger'
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import App from "./components/app/App";
 import root from './reducers'
 
-const store = createStore(root);
-
 render(
-    <Provider store={store}><App /></Provider>,
+    <Provider store={applyMiddleware(ReduxLogger)(createStore)(root)}>
+        <App />
+    </Provider>,
     document.getElementById('root'));
 
 registerServiceWorker();
