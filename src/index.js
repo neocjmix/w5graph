4ef6c98cdbe38417afa0e4e3b9654f1ffd333152
Chanjin Park <neocjmix@gmail.com>
2018-06-12 02:58:46 +0900

    middleware -> enhancer

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux'
-import {createStore, applyMiddleware} from 'redux'
+import {applyMiddleware, compose, createStore} from 'redux'
 import ReduxLogger from 'redux-logger'
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import App from "./components/app/App";
 import root from './reducers'
-import {restoreState} from "./shared/localStorageSynchronizer";
-import localStorageSynchronizer from "./shared/localStorageSynchronizer";
+import {restoreState, saveState} from "./shared/localStorageSynchronizer";
 
 const LOCAL_STORAGE_KEY = "state";
-const store = applyMiddleware(ReduxLogger, localStorageSynchronizer(LOCAL_STORAGE_KEY))(createStore)(root, restoreState(LOCAL_STORAGE_KEY));
+const store = createStore(
+    root,
+    compose(
+        applyMiddleware(ReduxLogger),
+        next => (reducer, initialState, enhancer) => {
+            const restoredState = restoreState(LOCAL_STORAGE_KEY);
+            const store = next(reducer, (restoredState == null) ? initialState: restoredState, enhancer);
+            store.subscribe(() => saveState(store.getState(), LOCAL_STORAGE_KEY));
+            return store;
+        }
+    )
+);
+
 render(
     <Provider store={store}>
         <App />
